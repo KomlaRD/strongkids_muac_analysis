bb0bb5723332a93dcd5d3583745b8c63e47f380a
KomlaRD <ankueric1@gmail.com>
2021-10-01 02:35:26 +0000

    Packages for project analysis

---
+++ packages.R
@@ -0,0 +1,19 @@
+## ---- packages
+
+pacman::p_load (
+        tidyverse, # Data management
+        rio, # Importing dataset
+        here, # Working directory
+        janitor, # Clean data names
+        googlesheets4, # Read sheets from Google sheets
+        gtsummary, # Create summary tables
+        skimr, # Overview of dataset
+        GGally, # Correlation matrix
+        tinytex, # Creating pdf documents
+        finalfit, # Creating models
+        ggthemes, # Plot themes
+        ggstatsplot, # Inferential plots
+        knitr # Chunk options
+        
+        
+)
